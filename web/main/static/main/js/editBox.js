1c77f4cd71940e9e4fc3d05991e8849bd570b1c1
JANGHEEEUN <qhqhclsrn@naver.com>
2020-05-04 14:53:06 +0900

    add editable function

--- web/main/static/main/js/editBox.js
+++ web/main/static/main/js/editBox.js
@@ -1,24 +1,40 @@
 var btnEdit = document.getElementById("edit");
 var form = document.getElementById("json_data").textContent;
 var ctx2;
+var canvasMain = document.getElementById("canvas-main");
+var objectList = document.getElementById("object-list");
 
-        var jsonForm = JSON.parse(form)
-        console.log("--------------JSON.parse(obj) result in EditBox ----------");
-        console.log(jsonForm);
+var jsonForm = JSON.parse(form)
+console.log("--------------JSON.parse(obj) result in EditBox ----------");
+console.log(jsonForm);
 
         for(key in jsonForm){
+            var form = document.createElement('form');
+            objectList.appendChild(form);
+
+            var div_kor = document.createElement('div');
+            div_kor.classList.add('form-group');
+            div_kor.innerText = jsonForm[key].txt;
+            div_kor.setAttribute("contenteditable", "true");
+            form.appendChild(div_kor);
+
+            var text_label = document.createElement('input');
+            text_label.type = 'text';
+            text_label.classList.add('form-control');
+            text_label.innerText = jsonForm[key].txt;
+            div_kor.appendChild(text_label);
+
             console.log(jsonForm[key].x);
-            var canvasMain = document.getElementById("canvas-main");
             var div = document.createElement('div');
-            div.style.color = "red";
-            div.innerText = key;
+            div.style.color = "black";
+            div.innerText = jsonForm[key].txt;
             div.style.left = jsonForm[key].x +"px";
             div.style.top = jsonForm[key].y +"px";
             div.style.width = jsonForm[key].w +"px";
             div.style.height = jsonForm[key].h + "px";
-            div.style.border = "2px solid black";
-            div.style.background="blue";
+            div.setAttribute("contenteditable", "true");
             div.style.overflow = "hidden";
             div.style.position = "absolute";
             canvasMain.appendChild(div);
+
         }
